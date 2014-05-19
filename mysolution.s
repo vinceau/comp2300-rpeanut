f5ae9176b4ed80a049ee7446558e96b13570b78a
Vince Au <vinceau09@gmail.com>
2014-05-20 00:16:54 +1000

    Joined draw and line functions (saved 3601 instructions).

--- mysolution.s
+++ mysolution.s
@@ -213,13 +213,11 @@ hextable:
 
 ;line: draws a line
 ;stack frame:
-;--|--|0 : y0 (modified)
-;--|0 |-1: x0 (modified)
-;0 |-1|-2: return address
-;-1|-2|-3: y1
-;-2|-3|-4: x1
-;-3|-4|-5: y0 (original)
-;-4|-5|-6: x0 (original)
+;0 : return address
+;-1: y1 (final y)
+;-2: x1 (final x)
+;-3: y0 (original)
+;-4: x0 (original)
 
 linedx:     block 1
 linedy:     block 1
@@ -228,89 +226,89 @@ linesy:     block 1
 lineerr:    block 1
 
 line:   load SP #-4 R0 ;R0 = x0
-        push R0 ;x0
-        load SP #-4 R1
-        push R1 ;y0
-
+        load SP #-3 R1 ;R1 = y0
+        
         ;dx = abs(x1-x0)
-        load SP #-4 R1 ;R1 = x1
-        jumplt R0 R1 line0
+        load SP #-2 R5 ;R5 = x1
+        ;if x0 < x1 then sx := 1 else sx := -1
+        jumplt R0 R5 line0
         store MONE linesx
         jump line1
 line0:  store ONE linesx
-line1:  sub R1 R0 R0 ;R0 = x1 - x0
+line1:  sub R5 R0 R6 ;R6 = x1 - x0
         ;abs(x1 - x0)
-        sub ZERO R0 R1 ;-(x1 - x0)
-        jumpn R1 linestoredx
-        move R1 R0
+        sub ZERO R6 R5 ;-(x1 - x0)
+        jumpn R5 linestoredx
+        move R5 R6
 linestoredx:
-        store R0 linedx ;update dx
+        store R6 linedx ;update dx
         ;dy = abs(y1 - y0)
-        load SP #0 R0 ;R0 = y0
-        load SP #-3 R1 ;R1 = y1
-        jumplt R0 R1 line2
+        load SP #-1 R5 ;R5 = y1
+        ;if y0 < y1 then sy := 1 else sy := -1
+        jumplt R1 R5 line2
         store MONE linesy
         jump line3
 line2:  store ONE linesy
-line3:  sub R1 R0 R0 ;R0 = y1 - y0
+line3:  sub R5 R1 R6 ;R6 = y1 - y0
         ;abs(y1 - y0)
-        sub ZERO R0 R1
-        jumpn R1 linestoredy
-        move R1 R0
+        sub ZERO R6 R5
+        jumpn R5 linestoredy
+        move R5 R6
 linestoredy:
-        store R0 linedy ;update dy
-        load linedx R1 ;R1 = dx
-        sub R1 R0 R0 ;R0 = dx - dy
-        store R0 lineerr; update err
+        store R6 linedy ;update dy
+        load linedx R5 ;R5 = dx
+        sub R5 R6 R6 ;R6 = dx - dy
+        store R6 lineerr; update err
 
         ;start loop
         jump line6
 
         ;if e2 > - dy
-line4:  load lineerr R0 ;R0 = err
-        add R0 R0 R1 ;(e2) R1 = 2 * err
-        load linedy R2 ;R2 = dy
-        mult MONE R2 R2 ;R2 = -dy
-        jumplte R1 R2 line5
+line4:  load lineerr R6 ;R6 = err
+        add R6 R6 R5 ;(e2) R5 = 2 * err
+        load linedy R4 ;R4 = dy
+        mult MONE R4 R4 ;R4 = -dy
+        jumplte R5 R4 line5
         ;err := err - dy
-        add R0 R2 R2 ;R2 = err + (-dy)
-        store R2 lineerr ;update err
+        add R6 R4 R4 ;R4 = err + (-dy)
+        store R4 lineerr ;update err
 
         ;x0 := x0 + sx
-        load SP #-1 R2 ;R2 = x0
         load linesx R3 ;R3 = sx
-        add R2 R3 R2 ;R2 = x0 + sx
-        store R2 #-1 SP ;update x0
+        add R0 R3 R0 ;R4 = x0 + sx
         
         ;if e2 < dx
-line5:  load lineerr R0 ;R0 = err
-        load linedx R2 ;R2 = dx
-        jumplte R2 R1 line6 ;go back to loop
+line5:  load lineerr R6 ;R6 = err
+        load linedx R4 ;R4 = dx
+        jumplte R4 R5 line6 ;go back to loop
         ; err := err + dx
-        add R0 R2 R2 ;R2 = err + dx
-        store R2 lineerr ;update err
+        add R6 R4 R4 ;R4 = err + dx
+        store R4 lineerr ;update err
         
         ; y0 := y0 + sy
-        load SP #0 R0 ;R0 = y0
-        load linesy R1 ;R1 = sy
-        add R0 R1 R1 ;R1 = y0 + sy
-        store R1 #0 SP ;update y0
+        load linesy R5 ;R5 = sy
+        add R1 R5 R1 ;R1 = y0 + sy
 
         ;loop again
-line6:  call draw ;setPixel
+line6:  ;setPixel
+        load #6 R2
+        mult R2 R1 R2 ;R2 = 6 * y
+        load #32 R3
+        div R0 R3 R3 ;R3 = x / 32
+        add R2 R3 R2 ;R2 = (6 * y) + (x / 32)
+        rotate R0 ONE R3 ;R3 is bit pattern
+        load R2 #0x7c40 R4 ;R2 = existing bit pattern
+        or R4 R3 R3 ;R3 = new bit pattern
+        store R3 #0x7c40 R2 ;R2 is displacement
 
         ;conditional
-        load SP #-1 R0  ;R0 = x0
-        load SP #-4 R1 ;R1 = x1
-        jumpneq R0 R1 line4 ;if x1 != x0
+        load SP #-2 R5 ;R5 = x1
+        jumpneq R0 R5 line4 ;if x1 != x0
         
         ;if x1 == x0
-        load SP #0 R0  ;R0 = y0
-        load SP #-3 R1 ;R1 = y1
-        jumpneq R0 R1 line4 ;loop if y1 != y0
+        load SP #-1 R5 ;R5 = y1
+        jumpneq R1 R5 line4 ;loop if y1 != y0
         ; exit loop
-        pop MONE ;y0
-        pop MONE ;x0
         return
 
 
